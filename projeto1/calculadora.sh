b9b41e130359a7144b2b048a5134d3663337e43d
Fernando457281960 <fndo0oliveira@gmail.com>
2024-12-28 11:09:05 -0300

    Initial commit

---
+++ projeto1/calculadora.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Iniciando o Script!"
+
+# Instalando o Python
+sudo apt update
+sudo apt install python3
+
+# Executando o projeto do módulo anterior
+python3 calculadora.py
+
+echo "Fim do Script!"
